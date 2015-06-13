fee96918ac5a59601ddf37ac75b4f205823eb340
Aloys OUVRARD <aloys.ouvrard@gmail.com>
2015-06-13 17:18:58 +0200

    Fix error : void --> float for getLoad function

--- loadcell-sigfox.ino
+++ loadcell-sigfox.ino
@@ -14,43 +14,42 @@ Akeru_ akeru;
 // Load cells constants 
 // Load is calculated by interpolation between two data pairs since the sensor is linear
 // First data pair
-const float loadA = 0; 			// Load A
-const int analogvalA = 512.5; 	// Analog reading taken with load A on the load cell
+const float loadA = 0;         // Load A
+const int analogvalA = 618;    // Analog reading taken with load A on the load cell
 
 // Second data pair
-const float loadB = 500;		// Load B
-const int analogvalB = 661.5; 	// Analog reading taken with load B on the load cell
+const float loadB = 500;       // Load B
+const int analogvalB = 819;    // Analog reading taken with load B on the load cell
 
 // Load variables
-float tare;						// Tare weight of the food container
-float currentLoad = 0;			// Current weight on the load cell (food+container)
-float load = 0;					// Food weight in the container
-const int nSamples = 30;		// Number of samples for the load value
+float tare;                    // Tare weight of the food container
+float currentLoad = 0;         // Current weight on the load cell (food+container)
+float load = 0;                // Food weight in the container
+const int nSamples = 30;       // Number of samples for the load value
 
 // Time variables
-unsigned long time = 0;						// Current time initialization
-const unsigned long timeoffset = 620000; 	// Time offset to send data
+unsigned long time = 0;                    // Current time initialization
+const unsigned long timeoffset = 620000;   // Time offset to send data
 
 // Initialization
 void setup() {
-	// Turn the LED on during the initialization process
-	digitalWrite(13, HIGH);
-	// Sigfox modem initilization
-	akeru.begin();
-    // Waiting for complete modem initialization
-	delay(2000);
+  // Turn the LED on during the initialization process
+  digitalWrite(13, HIGH);
+  // Sigfox modem initilization
+  akeru.begin();
+  // Waiting for complete modem initialization
+  delay(2000);
 	
- 	// Tare the food container weight
-	// BTW, It must be empty!
-	tare = getLoad();
+  // Tare the food container weight
+  // BTW, It must be empty!
+  tare = getLoad();
 
-	// Turn the LED off after the initialization process
-	digitalWrite(13, LOW);
+  // Turn the LED off after the initialization process
+  digitalWrite(13, LOW);
 }
 
 // Core
 void loop() {
-
   // Time to send data ?
   if(millis() > time + timeoffset){
     // Get the current weigth of the food in the container
@@ -61,36 +60,36 @@ void loop() {
 	// This is caused by a bad calibration during the initialization process (maybe...)
 	// TODO : fix it !
     if(load < 0)
-		load = 0;
+      load = 0;
 
-	// Turn the LED on during data sending
+    // Turn the LED on during data sending
     digitalWrite(13, HIGH);
     
-	// Send data to the Sigfox network
+    // Send data to the Sigfox network
     akeru.send(&load, sizeof(load));
 
-	// Turn the LED on during when data is send
+    // Turn the LED on during when data is send
     digitalWrite(13, LOW);
 
-	// Store the current time
+    // Store the current time
     time = millis();
   }
 }
 
 // Get the load from analog input
 // Smooth the data : average value of n samples (n=nSamples)
-void getLoad()
+float getLoad()
 {
-	int i;
-	int total = 0;
-    for (i = 0; i < nSamples; i++) {
-		// Read the analog input (the sensor)
-		total += analogRead(A0);
-	
-		delay(10);
-    }
-    
-	return analogToLoad(total / nSamples);
+  int i;
+  int total = 0;
+  
+  for (i = 0; i < nSamples; i++) {
+    // Read the analog input (the sensor)
+    total += analogRead(A0);
+    delay(10);
+  }
+
+  return analogToLoad(total / nSamples);
 }
 
 // Convert analog value to load using the two interpolation pairs
